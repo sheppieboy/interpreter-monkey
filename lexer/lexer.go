d10d6fdcd8433496ab654c3d1931c0511fc080aa
Luke Sheppard <sheppiegit@gmail.com>
2024-05-08 16:16:21 -0400

    added look up for keywords, so we can assign the correct identifier.

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -54,6 +54,7 @@ func (l *Lexer) NextToken() token.Token{
 	default:
 		if isLetter(l.ch){
 			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdentifier(tok.Literal)
 			return tok
 		}else{
 			tok = newToken(token.ILLEGAL, l.ch)
