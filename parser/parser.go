476b90dc1ad33a7d509ce602f5034a5b2dd0f3d1
Luke Sheppard <sheppiegit@gmail.com>
2024-05-08 20:12:05 -0400

    Test for the let statement in parser and parsing the let statement.

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+	"interpreter-monkey/ast"
 	"interpreter-monkey/lexer"
 	"interpreter-monkey/token"
 )
@@ -22,4 +24,66 @@ func New(l *lexer.Lexer) *Parser{
 func (p *Parser) nextToken(){
 	p.currToken = p.peekToken
 	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+	for p.currToken.Type != token.EOF{
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.currToken.Type{
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement()*ast.LetStatement{
+	stmt := &ast.LetStatement{Token: p.currToken}
+
+	if !p.expectPeek(token.IDENT){
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.currToken, Value: p.currToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN){
+		return nil
+	}
+
+	if !p.currTokenIs(token.SEMICOLON){
+		p.nextToken()
+	}
+
+
+	fmt.Println(stmt)
+	return stmt
+}
+
+func (p *Parser) currTokenIs(t token.TokenType) bool{
+	return p.currToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool{
+	if p.peekTokenIs(t){
+		p.nextToken()
+		return true
+	}else{
+		return false
+	}
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool{
+	return p.peekToken.Type == t
 }
