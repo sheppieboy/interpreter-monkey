3dcc087e088b16c230de3de786dbe4002513c2c7
Luke Sheppard <sheppiegit@gmail.com>
2024-05-10 11:01:15 -0400

    Added infixExpression ast node in ast.go, then added a precedence table in the parser.go file with helper methods to peek precedence of a token and get the current tokens precedence.

--- parser/parser.go
+++ parser/parser.go
@@ -20,6 +20,31 @@ const (
 	CALL // fn(x)
 )
 
+var precedences = map[token.TokenType]int{
+	token.EQ: EQUALS,
+	token.NOT_EQ: EQUALS,
+	token.LT: LESSGREATER,
+	token.GT: LESSGREATER,
+	token.PLUS:	SUM,
+	token.MINUS: SUM,
+	token.SLASH: PRODUCT,
+	token.ASTERISK: PRODUCT,
+}
+
+func (p *Parser) peekPrecedence() int {
+	if p, ok := precedences[p.peekToken.Type]; ok{
+		return p
+	}
+	return LOWEST
+}
+
+func (p *Parser) currPrecedence() int {
+	if p, ok := precedences[p.currToken.Type]; ok{
+		return p
+	}
+	return LOWEST
+}
+
 type Parser struct{
 	l *lexer.Lexer
 	currToken token.Token
