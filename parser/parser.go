c2dd225377388c9f74082255b6cb862832690554
Luke Sheppard <sheppiegit@gmail.com>
2024-05-11 16:05:09 -0400

    Added parsing for call expressions, updated the precedence for the call expression.  The call expression parsing is made of two functions parseCallExpression and parseCallArguments.

--- parser/parser.go
+++ parser/parser.go
@@ -29,6 +29,7 @@ var precedences = map[token.TokenType]int{
 	token.MINUS: SUM,
 	token.SLASH: PRODUCT,
 	token.ASTERISK: PRODUCT,
+	token.LPAREN: CALL,
 }
 
 func (p *Parser) peekPrecedence() int {
@@ -78,6 +79,7 @@ func New(l *lexer.Lexer) *Parser{
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.NOT_EQ, p.parseInfixExpression)
 	p.registerInfix(token.EQ, p.parseInfixExpression)
+	p.registerInfix(token.LPAREN, p.parseCallExpression)
 	return p
 }
 
@@ -359,4 +361,36 @@ func (p *Parser) parseFunctionParameters() []*ast.Identifier{
 		return nil
 	}
 	return identifiers
+}
+
+func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
+	exp := &ast.CallExpression{Token: p.currToken, Function: function}
+	exp.Arguments = p.parseCallArguments()
+	return exp
+}
+
+func (p *Parser) parseCallArguments() []ast.Expression{
+	args := []ast.Expression{}
+
+	//hit end of args when next token is RPAREN
+	if p.peekTokenIs(token.RPAREN){
+		p.nextToken()
+		return args
+	}
+
+	p.nextToken()
+	args = append(args, p.parseExpression(LOWEST))
+
+	for p.peekTokenIs(token.COMMA){
+		p.nextToken()
+		p.nextToken()
+		args = append(args, p.parseExpression(LOWEST))
+	}
+
+	if !p.expectPeek(token.RPAREN){
+		return nil
+	}
+
+	return args
+
 }
