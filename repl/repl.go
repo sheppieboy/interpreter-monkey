6b8ccd292f0599560be06808c052972fd246819b
Luke Sheppard <sheppiegit@gmail.com>
2024-05-11 17:54:30 -0400

    updated repl to use the parser.

--- repl/repl.go
+++ repl/repl.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"interpreter-monkey/lexer"
-	"interpreter-monkey/token"
+	"interpreter-monkey/parser"
 	"io"
 )
 
@@ -22,9 +22,22 @@ func Start(in io.Reader, out io.Writer){
 		line := scanner.Text()
 
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken(){
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0{
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string){
+	for _, msg := range errors{
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
