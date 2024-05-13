3b5562040cd4c5bb0ec449182494456fdeba0a69
Luke Sheppard <sheppiegit@gmail.com>
2024-05-13 11:49:28 -0400

    Added constant objects to represent true, false, and null to reduce memory usage as we don't want to create new objects for them each time.

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"interpreter-monkey/object"
 )
 
+var (
+	TRUE = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+	NULL = &object.Null{}
+)
+
 func Eval(node ast.Node) object.Object{
 	switch node := node.(type){
 
@@ -32,4 +38,11 @@ func evalStatements(stmts []ast.Statement) object.Object{
 	}
 
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean{
+	if input{
+		return TRUE
+	}
+	return FALSE
 }
