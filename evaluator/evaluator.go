dbcc878deaa0f9641dab1b89c3cf2db294d2204b
Luke Sheppard <sheppiegit@gmail.com>
2024-05-14 16:44:25 -0400

    Added the builtin in function len for strings.  Used a mapping to call the correct function, did some error checking, added to evaluator.go, and tested in evaluator_test.go

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -246,11 +246,16 @@ func isError(obj object.Object) bool {
 }
 
 func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
-	val, ok := env.Get(node.Value)
-	if !ok{
-		return newError("identifier not found: "+ node.Value)
+	if val, ok := env.Get(node.Value); ok{
+		return val
 	}
-	return val
+
+	if builtin, ok := builtins[node.Value]; ok{
+		return builtin
+	}
+
+	return newError("identifier not found: "+ node.Value)
+
 }
 
 func evalExpressions(exps []ast.Expression, env *object.Environment)[]object.Object{
@@ -268,14 +273,18 @@ func evalExpressions(exps []ast.Expression, env *object.Environment)[]object.Obj
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
-	function, ok := fn.(*object.Function)
-	if !ok{
+	switch function := fn.(type){
+	case *object.Function:
+		extendEnv := extendedFunctionEnv(function, args)
+		evaluated := Eval(function.Body, extendEnv)
+		return unwrapReturnValue(evaluated)
+
+	case *object.Builtin:
+		return function.Fn(args...)
+
+	default:
 		return newError("not a function: %s", fn.Type())
 	}
-
-	extendEnv := extendedFunctionEnv(function, args)
-	evaluated := Eval(function.Body, extendEnv)
-	return unwrapReturnValue(evaluated)
 }
 
 func extendedFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
