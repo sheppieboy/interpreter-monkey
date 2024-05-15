99431446cee1dc1a17ecd57c87712173d5498ead
Luke Sheppard <sheppiegit@gmail.com>
2024-05-15 17:47:42 -0400

    Added the ast structure for the HashLiteral data type.  Added several tests to define the parsing behaviour for a hash literal data type.  Then updated the parser to properly parse the Hash literal data type.

--- parser/parser.go
+++ parser/parser.go
@@ -73,6 +73,7 @@ func New(l *lexer.Lexer) *Parser{
 	p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
 	p.registerPrefix(token.STRING, p.parseString)
 	p.registerPrefix(token.LBRACKET, p.parseArrayLiteral)
+	p.registerPrefix(token.LBRACE, p.parseHashLiteral)
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
@@ -423,4 +424,31 @@ func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression{
 	}
 
 	return expr
+}
+
+func (p *Parser) parseHashLiteral() ast.Expression {
+	hash := &ast.HashLiteral{Token: p.currToken}
+	hash.Pairs = make(map[ast.Expression]ast.Expression)
+
+	for !p.peekTokenIs(token.RBRACE){
+		p.nextToken()
+		key := p.parseExpression(LOWEST)
+
+		if !p.expectPeek(token.COLON){
+			return nil
+		}
+		p.nextToken()
+		value := p.parseExpression(LOWEST)
+		hash.Pairs[key] = value
+
+		if !p.peekTokenIs(token.RBRACE) && !p.expectPeek(token.COMMA){
+			return nil
+		}
+	}
+
+	if !p.expectPeek(token.RBRACE){
+		return nil
+	}
+
+	return hash
 }
