a2d84759b6e3597df7df9e23dfedf5193bab4f54
Luke Sheppard <sheppiegit@gmail.com>
2024-05-15 16:42:49 -0400

    Added the array builtin functions: first, last, rest, push, rest.  Added tests for them.

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -306,8 +306,21 @@ func TestBuiltinFunctions(t *testing.T){
 		{`len("")`, 0},
 		{`len("four")`, 4},
 		{`len("hello world")`, 11},
-		{`len(1)`, "argument to `len` not supported, got INTEGER"},
+		{`len(1)`, "argument to `len` not supported, got=INTEGER"},
 		{`len("one", "two")`, "wrong number of arguments. got=2, want=1"},
+		{`len([1, 2, 3])`, 3},
+		{`len([])`, 0},
+		{`puts("hello", "world!")`, nil},
+		{`first([1, 2, 3])`, 1},
+		{`first([])`, nil},
+		{`first(1)`, "argument to `first` must be ARRAY, got=INTEGER"},
+		{`last([1, 2, 3])`, 3},
+		{`last([])`, nil},
+		{`last(1)`, "argument to `last` must be ARRAY, got=INTEGER"},
+		{`rest([1, 2, 3])`, []int{2, 3}},
+		{`rest([])`, nil},
+		{`push([], 1)`, []int{1}},
+		{`push(1, 1)`, "argument to `push` must be ARRAY, got=INTEGER"},
 	}
 
 	for _, tt := range tests{
@@ -316,6 +329,24 @@ func TestBuiltinFunctions(t *testing.T){
 		switch expected := tt.expected.(type){
 		case int:
 			testIntegerObject(t, evaluated, int64(expected))
+		case nil:
+			testNullObject(t, evaluated)
+		case []int:
+			array, ok := evaluated.(*object.Array)
+			if !ok {
+				t.Errorf("obj not Array. got=%T (%+v)", evaluated, evaluated)
+				continue
+			}
+
+			if len(array.Elements) != len(expected) {
+				t.Errorf("wrong num of elements. want=%d, got=%d",
+					len(expected), len(array.Elements))
+				continue
+			}
+
+			for i, expectedElem := range expected {
+				testIntegerObject(t, array.Elements[i], int64(expectedElem))
+			}
 		case string:
 			errObj, ok := evaluated.(*object.Error)
 			if !ok{
@@ -330,8 +361,76 @@ func TestBuiltinFunctions(t *testing.T){
 	}
 }
 
+// func TestArrayBuiltinFunctions(t *testing.T){
+// 	tests := []struct{
+// 		input string
+// 		expected interface{}
+// 	}{
+// 		{
+// 			`let arr = [1, 2, 3]; len(arr);`,
+// 			3,
+// 		},
+// 		{
+// 			`len([1,2]);`,
+// 			2,
+// 		},
+// 		{
+// 			`len([])`,
+// 			0,
+// 		},
+// 		{
+// 			`let arr = [1,2,3]; first(arr);`,
+// 			1,
+// 		},
+// 		{
+// 			`first([1, 2]);`, 
+// 			1,
+// 		},
+// 		{
+// 			`first([]);`,
+// 			nil,
+// 		},
+// 		{
+// 			`first([true, false])`,
+// 			true,
+// 		},
+// 		{
+// 			`let arr = [1, false, 2]; last(arr)`,
+// 			2,
+// 		},
+// 		{
+// 			`last([1,2, 3])`,
+// 			3,
+// 		},
+// 		{
+// 			`last([false, 1, true]);`,
+// 			true,
+// 		},
+// 		{
+// 			`last([])`,
+// 			nil,
+// 		},
+// 		{
+// 			`last([1,2, 3])`,
+// 			3,
+// 		},
+		
+// 	}
+
+// 	for _, tt := range tests {
+// 		evaluated := testEval(tt.input)
+
+// 		switch expected := tt.expected.(type){
+// 		case int:
+// 			testIntegerObject(t, evaluated, int64(expected))
+// 		case bool:
+// 			testBooleanObject(t, evaluated, expected)
+// 		}
+// 	}
+// }
+
 func TestArrayLiterals(t *testing.T){
-	input := `[1, 2 * 2, 3 + 3, true]`
+	input := `[1, 2 * 2, 3 + 3]`
 
 	evaluated := testEval(input)
 	result, ok := evaluated.(*object.Array)
@@ -345,7 +444,6 @@ func TestArrayLiterals(t *testing.T){
 	testIntegerObject(t, result.Elements[0], 1)
 	testIntegerObject(t, result.Elements[1], 4)
 	testIntegerObject(t, result.Elements[2], 6)
-	testBooleanObject(t, result.Elements[3], true)
 }
 
 func TestArrayIndexExpressions(t *testing.T){
