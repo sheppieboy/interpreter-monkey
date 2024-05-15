bcf9b29543a633799063bcac3bf95096a851368b
Luke Sheppard <sheppiegit@gmail.com>
2024-05-15 18:40:26 -0400

    Added Hash literal evaluations. Added several tests to check the validity of evaluating the hash literal.  Updated object with a valid hashable literal that retrieves in O(1) time.  Added a test in object to double check pointer comparison.  Updated the evalautor.go file to evaluate the hash literal properly.

--- object/object.go
+++ object/object.go
@@ -3,6 +3,7 @@ package object
 import (
 	"bytes"
 	"fmt"
+	"hash/fnv"
 	"interpreter-monkey/ast"
 	"strings"
 )
@@ -17,6 +18,7 @@ const (
 	STRING_OBJ = "STRING"
 	BUILTIN_OBJ = "BUILTIN"
 	ARRAY_OBJ = "ARRAY_OBJ"
+	HASH_OBJ = "HASH_OBJ"
 )
 
 type ObjectType string
@@ -150,6 +152,64 @@ func (a *Array) Inspect() string {
 	out.WriteString("]")
 
 	return out.String()
+}
+
+type HashKey struct {
+	Type ObjectType
+	Value uint64
+}
+
+func (b *Boolean) HashKey() HashKey{
+	var value uint64
+
+	if b.Value {
+		value = 1
+	}else{
+		value = 0
+	}
+	return HashKey{Type: b.Type(), Value: value}
+}
+
+func (i *Integer) HashKey() HashKey{
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
+
+func (s *String) HashKey() HashKey{
+	h := fnv.New64a()
+	h.Write([]byte(s.Value))
+	return HashKey{Type: s.Type(), Value: h.Sum64()}
+}
+
+type HashPair struct {
+	Key Object
+	Value Object
+}
+
+type Hash struct{
+	Pairs map[HashKey]HashPair
+}
+
+func (h *Hash) Type() ObjectType{
+	return HASH_OBJ
+}
+
+func (h * Hash) Inspect() string {
+	var out bytes.Buffer
+
+	pairs :=[]string{}
+
+	for _, pair := range h.Pairs {
+		pairs = append(pairs, fmt.Sprintf("%s: %s", pair.Key.Inspect(), pair.Key.Type()))
+	}
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
+
+	return out.String()
+}
+
+type Hashable interface{
+	HashKey() HashKey
 }
 
 
