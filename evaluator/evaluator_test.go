a03eb16bade4305927d1ce790480a63025051f86
Luke Sheppard <sheppiegit@gmail.com>
2024-05-15 20:16:49 -0400

    Updated evaluator with evalHashIndexExpression.  This function takes the hash and index object and returns hash object.  Added several tests to check the correctness of the index access operator for the hash literal.

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -182,6 +182,10 @@ func TestErrorHandling(t *testing.T){
 			`"Hello" - "World"`,
 			"unknown operator: STRING - STRING",
 		},
+		{
+			`{"name": "Monkey"}[fn(x){ x }]`,
+			"unusable as hash key: FUNCTION",
+		},
 	}
 	for _, tt := range tests{
 		evaluated := testEval(tt.input)
@@ -378,6 +382,52 @@ func TestArrayLiterals(t *testing.T){
 	testIntegerObject(t, result.Elements[0], 1)
 	testIntegerObject(t, result.Elements[1], 4)
 	testIntegerObject(t, result.Elements[2], 6)
+}
+
+func TestHashIndexExpressions(t *testing.T){
+	tests := []struct{
+		input string
+		expected interface{}
+	}{
+		{
+			`{"foo": 5}["foo"];`,
+			5,
+		},
+		{
+			`{"foo": 5}["bar"]`,
+			nil,
+		},
+		{
+			`let key = "foo";{"foo":5}[key]`,
+			5,
+		},
+		{
+			`{}["foo"]`,
+			nil,
+		},
+		{
+			`{5: 5}[5]`,
+			5,
+		},
+		{
+			`{true: 5}[true]`,
+			5,
+		},
+		{
+			`{false: 5}[false]`,
+			5,
+		},
+	}
+
+	for _, tt := range tests{
+		evaluated := testEval(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		}else{
+			testNullObject(t, evaluated)
+		}
+	}
 }
 
 func TestArrayIndexExpressions(t *testing.T){
